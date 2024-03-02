d1a95f87f3c5c28001cfb6f19834be143e6e7ae6
ColinAdo <colinadodev30@gmail.com>
2024-03-02 21:05:22 +0300

    Add timetable model

--- timetables/admin.py
+++ timetables/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from timetables.models import Timetable
+
+class TimetableAdmin(admin.ModelAdmin):
+    list_display = [
+        "owner", 
+        "day_of_week", 
+        "unit_code", 
+        "unit_name", 
+        "mode_of_study",
+        "group"
+    ]
+
+admin.site.register(Timetable, TimetableAdmin)
--- timetables/models.py
+++ timetables/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Timetable(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        related_name='timetables', 
+        on_delete=models.CASCADE
+        )
+    day_of_week = models.CharField(max_length=200)
+    lesson_time = models.CharField(max_length=200)
+    unit_code = models.CharField(max_length=200)
+    unit_name = models.CharField(max_length=200)
+    lecturer = models.CharField(max_length=200, blank=True)
+    campus = models.CharField(max_length=200, blank=True)
+    mode_of_study = models.CharField(max_length=200, default='regular')
+    room = models.CharField(max_length=200, blank=True)
+    group = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.owner.username} timetable"
